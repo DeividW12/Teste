44816d162a1f79946041abd27d6413598d882cdd
DeividW12 <124920237+DeividW12@users.noreply.github.com>
2023-02-09 16:29:49 -0300

    Teste de commit via visual

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor");
+
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro valor");
+
+            int y = int.Parse(Console.ReadLine());
+
+            int soma = x + y;
+
+            Console.WriteLine("A soma dos valores {0} e {1} é: {2}", x, y, soma);
+
+            Console.WriteLine("Digite um valor para multiplcar a soma");
+
+            int mult = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A multiplicação da soma é: {0} ", soma * mult);
+
+            Console.ReadLine();
+        }
+    }
+}
