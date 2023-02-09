ae3a543f8252825223ff72883ff724531c4ab894
DeividW12 <124920237+DeividW12@users.noreply.github.com>
2023-02-09 16:29:49 -0300

    Add files via upload
    Adicionado arquivos pelo site

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor");
+
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro valor");
+
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A soma dos valores {0} e {1} é: {2}", x, y, x+y);
+
+                        Console.ReadLine();
+        }
+    }
+}
